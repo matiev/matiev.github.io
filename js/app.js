f6e90879984ebda9d74367ab5ffeffe9475b24c5
matiev <matiev@live.com>
2023-07-02 13:06:22 +0300

    add random border-color linear-gradient

--- js/app.js
+++ js/app.js
@@ -6,5 +6,4 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   wrapper.style.borderImage = `linear-gradient(to right, #222, #222, #222, #222, rgb(${color}))`;
   wrapper.style.borderImageSlice = `1`;
-  
 });
