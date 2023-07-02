41216cc2d21476d65fe9340fd2c8c21a50d9c345
matiev <matiev@live.com>
2023-07-02 13:06:02 +0300

    add random border-color linear-gradient

---
+++ js/app.js
@@ -0,0 +1,9 @@
+let wrapper = document.querySelector('.wrapper');
+let color = [];
+window.addEventListener('DOMContentLoaded', () => {
+  for (let i=0; i<=2; i++) {
+    color.push(Math.floor(Math.random() * 255));
+  }
+  wrapper.style.borderImage = `linear-gradient(to right, #222, #222, #222, #222, rgb(${color}))`;
+  wrapper.style.borderImageSlice = `1`
+});
